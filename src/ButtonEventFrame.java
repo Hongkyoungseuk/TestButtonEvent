81e7a98d7559fba9b677979b58c25073a5f1b9bd
{Hongkyoungseuk} <tug234@daum.net>
2023-10-06 12:29:32 +0900

    10.06 12:29

--- src/ButtonEventFrame.java
+++ src/ButtonEventFrame.java
@@ -11,21 +11,16 @@ public class ButtonEventFrame extends Frame implements ActionListener {
         this.setSize(200, 200);
 
         Button button1 = new Button();
-        Button button2 = new Button();
         button1.setBounds(getWidth() / 2 - 50, 30, 100, 40);
         button1.setLabel("Click_1!!!");
-        button2.setBounds(getWidth() / 2 - 50, 100, 100, 40);
-        button2.setLabel("Click_2!!!");
+
         // ActionEvent 리스너를 등록
         button1.addActionListener(this);
         add(button1);
-        button2.addActionListener(this);
-        add(button2);
         setLayout(null);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        // todo
         e.getModifiers();
 
         System.out.println("Button : " + e + " " + ++count);
